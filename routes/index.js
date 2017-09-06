e043a24fdb0fd9973022c4ef53200ffef3e46e03
andreblgithub <andrewbl@icloud.com>
2017-09-06 13:12:12 -0400

    app ready

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var data = {"ipaddress":"173.220.229.173","language":"en-US","software":"Windows NT 6.1; Win64; x64"};
+  var ip = req.connection.remoteAddress;
+  ip = ip.replace(/^.*:/, '');
+  var lang = req.headers["accept-language"];
+  lang = lang.substring(0, lang.indexOf(','));
+  var soft = req.headers["user-agent"];
+  soft = soft.substring(soft.indexOf('(') + 1, soft.indexOf(')'));
+  //{"ipaddress":"173.220.229.173","language":"en-US","software":"Windows NT 6.1; Win64; x64"};
+  var data = {"ipaddress":ip,"language":lang,"software":soft};
   res.json(data);
 });
 
