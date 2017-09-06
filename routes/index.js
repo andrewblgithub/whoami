11e186e38fac60a88e3d071c4e0c711c5291da20
andreblgithub <andrewbl@icloud.com>
2017-09-06 13:19:09 -0400

    ip address fix

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var ip = req.connection.remoteAddress;
+  var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
   ip = ip.replace(/^.*:/, '');
   var lang = req.headers["accept-language"];
   lang = lang.substring(0, lang.indexOf(','));
